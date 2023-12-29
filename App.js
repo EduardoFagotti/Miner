f191a909fa25962dd956dc9c2a67c8c4c4861b8e
Eduardo Fagotti <fagotti.edu567@outlook.com>
2023-12-29 09:05:33 -0300

    parametros de estilo

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import params from "./src/params";
+import Field from "./src/components/Field";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>iniciando o Mines!</Text>
+      <Text>
+        Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+      </Text>
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={4} />
+      <Field opened nearMines={5} />
+      <Field opened nearMines={6} />
     </View>
   );
 }
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
