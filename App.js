da1e63b66a61b97364320af19edd1f5fb9c9f8cc
Eduardo Fagotti <fagotti.edu567@outlook.com>
2024-01-07 11:12:10 -0300

    camp

--- App.js
+++ App.js
@@ -1,37 +1,54 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import params from "./src/params";
-import Field from "./src/components/Field";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>iniciando o Mines!</Text>
-      <Text>
-        Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
-      </Text>
-      <Field />
-      <Field opened />
-      <Field opened nearMines={1} />
-      <Field opened nearMines={2} />
-      <Field opened nearMines={3} />
-      <Field opened nearMines={4} />
-      <Field opened nearMines={5} />
-      <Field opened nearMines={6} />
-      <Field mined />
-      <Field mined opened />
-      <Field mined opened exploded />
-      <Field flagged />
-      <Field flagged opened />
-    </View>
-  );
+import MineField from "./src/components/MineField";
+import { createMinedBoard } from "./src/functions";
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: createMinedBoard(
+        params.getRowsAmount(),
+        params.getColumnsAmount(),
+        this.minesAmount()
+      ),
+    };
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultlevel);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>iniciando o Mines!</Text>
+        <Text>
+          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    justifyContent: "flex-end",
+  },
+
+  board: {
     alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#aaa",
   },
 });
+
+export default App;
